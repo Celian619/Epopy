85a6aa5bff7b75de072787f7452f3549fabcb47d
Celi0n <celian.g@orange.fr>
2017-09-03 23:14:23 +0200

    Design Tetris

--- net/epopy/epopy/games/tetras/menus/TetrasGame.java
+++ net/epopy/epopy/games/tetras/menus/TetrasGame.java
@@ -1,5 +1,7 @@
 package net.epopy.epopy.games.tetras.menus;
 
+import static org.lwjgl.opengl.GL11.glColor4f;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -13,75 +15,75 @@ import net.epopy.epopy.utils.Input;
 import net.epopy.epopy.utils.Location;
 
 public class TetrasGame extends AbstractGameMenu {
-
+	
 	private static final int grilleSectionX = 40;
 	private static final int grilleSectionY = 30;
 	private static final double grilleWidth = defaultWidth / grilleSectionX;
 	private static final double grilleHeight = defaultHeight / grilleSectionY;
 	private double fallSpeed = 0.075;
-	
+
 	private boolean[] isBlock;
 	private boolean right;
 	private boolean left;
 	private boolean down;
 	private boolean rotateL;
 	private boolean rotateR;
-
+	
 	private boolean lastRotateL;
 	private boolean lastRotateR;
 	private boolean changeStopDown;
-	
-	private int score;
 
-	private block movingBlock;
+	private int score;
 	
+	private block movingBlock;
+
 	float[] color;
 	float[] lastColor;
-	
+
 	@Override
 	public void onEnable() {
 		score = 0;
 		changeStopDown = false;
 		Random r = new Random();
-		color = new float[] { r.nextFloat(), r.nextFloat(), r.nextFloat(), 1f };
-		lastColor = new float[] { r.nextFloat(), r.nextFloat(), r.nextFloat(), 1f };
-		
+		color = new float[] { (float) (0.5 + r.nextFloat() / 2), (float) (0.5 + r.nextFloat() / 2), (float) (0.5 + r.nextFloat() / 2), 1f };
+		lastColor = new float[] { (float) (0.5 + r.nextFloat() / 2), (float) (0.5 + r.nextFloat() / 2), (float) (0.5 + r.nextFloat() / 2), 1f };
+
 		isBlock = new boolean[grilleSectionX * grilleSectionY];
 		movingBlock = new block(new Location(grilleSectionX / 2, 2));
-
+		
 	}
-
+	
 	@Override
 	public void update() {
-
+		
 		if (rotateL)
 			lastRotateL = true;
 		if (rotateR)
 			lastRotateR = true;
-
+			
 		right = false;
 		left = false;
 		down = false;
 		rotateL = false;
 		rotateR = false;
-
+		
 		if (Keyboard.isKeyDown(Keyboard.KEY_RIGHT)) {
 			right = true;
-
-		}
-
-		if (Keyboard.isKeyDown(Keyboard.KEY_LEFT)) {
-			left = true;
 			
 		}
+		
+		if (Keyboard.isKeyDown(Keyboard.KEY_LEFT)) {
+			left = true;
 
+		}
+		
 		if (Keyboard.isKeyDown(Keyboard.KEY_DOWN)) {
 			if (!changeStopDown)
 				down = true;
 		} else if (changeStopDown) {
 			changeStopDown = false;
 		}
-
+		
 		if (Input.isButtonDown(0)) {
 			if (!lastRotateL) {// juste un clic
 				rotateL = true;
@@ -90,7 +92,7 @@ public class TetrasGame extends AbstractGameMenu {
 		} else {
 			lastRotateL = false;
 		}
-
+		
 		if (Input.isButtonDown(1)) {
 			if (!lastRotateR) {// juste un clic
 				rotateR = true;
@@ -99,54 +101,68 @@ public class TetrasGame extends AbstractGameMenu {
 		} else {
 			lastRotateR = false;
 		}
-
+		
 		movingBlock.move();
-
+		
 	}
-
+	
 	@Override
 	public void render() {
-		
+
 		Textures.GAME_TETRAS_BG.renderBackground();
 		Location locat = movingBlock.loc;
-		
+
 		for (Location loc : movingBlock.locs) {
 			double x = loc.getX() + locat.getX();
 			double y = loc.getY() + locat.getY();
-			
+
 			x *= grilleWidth;
 			y *= grilleHeight;
 			
-			ComponentsHelper.drawQuad((int) x - 1, (int) y - 1, (int) grilleWidth + 2, (int) grilleHeight + 2, lastColor);
-			ComponentsHelper.drawQuad((int) x + 1, (int) y + 1, (int) grilleWidth - 2, (int) grilleHeight - 2, color);
+			glColor4f(color[0], color[1], color[2], color[3]);
+			ComponentsHelper.renderTexture(Textures.GAME_TETRAS_BLOCK, x - 1, y - 1, (int) grilleWidth + 2, (int) grilleHeight + 2);
+			
+			glColor4f(1, 1, 1, 1);
+			/*
+			 * ComponentsHelper.drawQuad((int) x - 1, (int) y - 1, (int) grilleWidth + 2, (int) grilleHeight + 2, lastColor);
+			 * ComponentsHelper.drawQuad((int) x + 1, (int) y + 1, (int) grilleWidth - 2, (int) grilleHeight - 2, color);
+			 */
 		}
 		int i = 0;
 		for (Boolean b : isBlock) {
 			if (b) {
 				int y = i / grilleSectionX;
 				int x = i - y * grilleSectionX;
-				
+
 				x *= grilleWidth;
 				y *= grilleHeight;
-				ComponentsHelper.drawQuad(x - 1, y - 1, (int) grilleWidth + 2, (int) grilleHeight + 2, color);
-				ComponentsHelper.drawQuad(x + 1, y + 1, (int) grilleWidth - 2, (int) grilleHeight - 2, lastColor);
 				
+				glColor4f(lastColor[0], lastColor[1], lastColor[2], lastColor[3]);
+				
+				ComponentsHelper.renderTexture(Textures.GAME_TETRAS_BLOCK, x - 1, y - 1, (int) grilleWidth + 2, (int) grilleHeight + 2);
+				
+				glColor4f(1, 1, 1, 1);
+				/*
+				 * ComponentsHelper.drawQuad(x - 1, y - 1, (int) grilleWidth + 2, (int) grilleHeight + 2, color);
+				 * ComponentsHelper.drawQuad(x + 1, y + 1, (int) grilleWidth - 2, (int) grilleHeight - 2, lastColor);
+				 */
+
 			}
-			
+
 			i++;
 		}
-		
-		ComponentsHelper.drawText("Score : " + score, 10, 50, 40);
-		
-	}
 
+		ComponentsHelper.drawText("Score : " + score, 10, 50, 40);
+
+	}
+	
 	private boolean deathTest() {
 		for (int i = 0; i < grilleSectionX * 4; i++) {
 			if (isBlock[i]) return true;
 		}
 		return false;
 	}
-	
+
 	private void deleteLines() {
 		for (int y = 3; y < grilleSectionY; y++) {
 			boolean fullLine = true;
@@ -158,10 +174,10 @@ public class TetrasGame extends AbstractGameMenu {
 			}
 			if (fullLine) {
 				fallSpeed -= 0.01;
-
+				
 				boolean clearLine = true;
 				for (int yUp = y; yUp > 0; yUp--) {
-
+					
 					for (int xUp = grilleSectionX - 1; xUp >= 0; xUp--) {
 						if (isBlock[yUp * grilleSectionX + xUp] && clearLine) {
 							isBlock[yUp * grilleSectionX + xUp] = false;
@@ -174,21 +190,21 @@ public class TetrasGame extends AbstractGameMenu {
 				}
 			}
 		}
-
+		
 	}
-
+	
 	class block {
-
+		
 		Location loc;
 		List<Location> locs;
-
+		
 		private block(final Location loc) {
 			this.loc = loc;
 			Random r = new Random();
 			locs = new ArrayList<Location>(4);
 			Location locat = new Location(0, 0);
 			locs.add(locat);
-
+			
 			for (int i = 0; i < 3; i++) {
 				Location locR = locs.get(r.nextInt(locs.size()));
 				Location newLoc;
@@ -196,7 +212,7 @@ public class TetrasGame extends AbstractGameMenu {
 					newLoc = locR.clone().add(r.nextInt(3) - 1, 0);
 				else
 					newLoc = locR.clone().add(0, r.nextInt(3) - 1);
-
+					
 				boolean same = false;
 				for (Location location : locs) {
 					if (location.distance(newLoc) == 0) {
@@ -206,50 +222,50 @@ public class TetrasGame extends AbstractGameMenu {
 					}
 				}
 				if (same) continue;
-
+				
 				locs.add(newLoc);
 			}
 		}
-
+		
 		private void move() {
-
+			
 			if (rotateL) {
 				List<Location> newLocations = new ArrayList<Location>(4);
 				Boolean rotate = true;
 				for (Location locat : locs) {
 					double x = locat.getX();
 					double y = locat.getY();
-
+					
 					if (isCollision(new Location(-y + loc.getX(), x + loc.getY()))) {
 						rotate = false;
 						break;
 					}
 					newLocations.add(new Location(-y, x));
 				}
-
+				
 				if (rotate) {
 					locs = newLocations;
 				}
-
+				
 			} else if (rotateR) {
-
+				
 				List<Location> newLocations = new ArrayList<Location>(4);
 				Boolean rotate = true;
 				for (Location locat : locs) {
 					double x = locat.getX();
 					double y = locat.getY();
-
+					
 					if (isCollision(new Location(y + loc.getX(), -x + loc.getY()))) {
 						rotate = false;
 						break;
 					}
 					newLocations.add(new Location(y, -x));
 				}
-
+				
 				if (rotate) {
 					locs = newLocations;
 				}
-
+				
 			} else if (left) {
 				Location newLoc = new Location(loc.getX() - 0.2, loc.getY());
 				if (!isTetrisLeft(locs, newLoc)) {
@@ -266,16 +282,16 @@ public class TetrasGame extends AbstractGameMenu {
 					loc = newLoc;
 				}
 			}
-
+			
 			if (!left && !right && (int) loc.getX() != loc.getX()) {
 				if (loc.getX() - (int) loc.getX() > 0.5)
 					loc.setX((int) loc.getX() + 1);
 				else
 					loc.setX((int) loc.getX());
 			}
-
+			
 			// System.out.println(testTetrisCollision(locs, loc));
-
+			
 			Location newLoc = new Location(loc.getX(), loc.getY() + fallSpeed);
 			if (!isTetrisInGround(locs, newLoc)) {// peut descendre
 				loc = newLoc;
@@ -283,9 +299,9 @@ public class TetrasGame extends AbstractGameMenu {
 				for (Location locat : locs) {
 					int x = (int) (locat.getX() + loc.getX());
 					int y = (int) (locat.getY() + loc.getY());
-
+					
 					isBlock[y * grilleSectionX + x] = true;
-
+					
 				}
 				score++;
 				fallSpeed += 0.001;
@@ -297,64 +313,58 @@ public class TetrasGame extends AbstractGameMenu {
 					movingBlock = new block(new Location(grilleSectionX / 2, 2));
 					Random r = new Random();
 					lastColor = color;
-					color = new float[] { r.nextFloat(), r.nextFloat(), r.nextFloat(), 1f };
+					color = new float[] { (float) (0.5 + r.nextFloat() / 2), (float) (0.5 + r.nextFloat() / 2), (float) (0.5 + r.nextFloat() / 2), 1f };
 					changeStopDown = true;
 				}
-
+				
 			}
-
+			
 		}
-
+		
 		private boolean isTetrisLeft(final List<Location> locations, final Location locBase) {
-
-			// au sol
 
 			for (Location locs : locations) {
 				if (loc.getX() + locs.getX() < 0.2) return true;
 
-				int testCase = (int) (loc.getY() + locs.getY()) * grilleSectionX + (int) (loc.getX() + locs.getX() - 1);
+				int testCase = (int) (loc.getY() + locs.getY()) * grilleSectionX + (int) (loc.getX() + locs.getX() - 0.2);
 				if (testCase > 0 && testCase < grilleSectionX * grilleSectionY && isBlock[testCase])
 					return true;
 			}
 			return false;
 		}
-
+		
 		private boolean isTetrisRight(final List<Location> locations, final Location locBase) {
-
-			// au sol
 
 			for (Location locs : locations) {
 				if (loc.getX() + locs.getX() + 1 >= grilleSectionX) return true;
-
+				
 				int testCase = (int) (loc.getY() + locs.getY()) * grilleSectionX + (int) (loc.getX() + locs.getX() + 1);
 				if (testCase > 0 && testCase < grilleSectionX * grilleSectionY && isBlock[testCase])
 					return true;
 			}
 			return false;
 		}
-
+		
 		private boolean isTetrisInGround(final List<Location> locations, final Location locBase) {
-
-			// au sol
 
 			for (Location locs : locations) {
 				if (loc.getY() + locs.getY() + 1 >= grilleSectionY) return true;
-
+				
 				int testCase = (int) (loc.getY() + locs.getY() + 1) * grilleSectionX + (int) (loc.getX() + locs.getX());
 				if (testCase > 0 && testCase < grilleSectionX * grilleSectionY && isBlock[testCase])
 					return true;
 			}
 			return false;
 		}
-
+		
 		private boolean isCollision(final Location loca) {// y a t'il un block ?
 			int xTest = (int) loca.getX();
 			int yTest = (int) loca.getY();
 			// hors limite
 			if (xTest <= 0 || xTest >= grilleSectionX || yTest <= 0 || yTest >= grilleSectionY) return true;
-
+			
 			return isBlock[yTest * grilleSectionX + xTest];
-
+			
 		}
 	}
 }
