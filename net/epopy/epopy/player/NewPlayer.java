bc2254dd23d86619309dae803e47b9c73532bb45
Celi0n <celian.g@orange.fr>
2017-09-13 20:15:44 +0200

    Player level

--- net/epopy/epopy/player/NewPlayer.java
+++ net/epopy/epopy/player/NewPlayer.java
@@ -15,9 +15,9 @@ import net.epopy.epopy.Main;
 import net.epopy.epopy.utils.FileUtils;
 
 public class NewPlayer {
-
+	
 	public NewPlayer(final String name) {
-		
+
 		File profil = new File(FileUtils.PATH_FOLDER + name + ".txt");
 		try {
 			if (profil.exists()) {
@@ -27,29 +27,29 @@ public class NewPlayer {
 			FileOutputStream is = new FileOutputStream(profil);
 			OutputStreamWriter osw = new OutputStreamWriter(is);
 			Writer w = new BufferedWriter(osw);
-			
+
 			String timeStamp = new SimpleDateFormat("dd/MM/yyyy").format(Calendar.getInstance().getTime());
 			write(w, "account_create_at=" + timeStamp);
 			write(w, "display_fullscreen=true");
-			
-			write(w, "level=" + 10);// TODO Modifier ceci !
-			
+
+			write(w, "level=" + 1);// TODO Modifier ceci !
+
 			write(w, "last_game=" + 1);
-			
+
 			/*
 			 * Snake
 			 */
 			write(w, "snake_pts=0");
 			write(w, "snake_parties=0");
 			write(w, "snake_temps=0");
-			
+
 			/*
 			 * Pong
 			 */
 			write(w, "ping_record=0");
 			write(w, "ping_parties=0");
 			write(w, "ping_temps=0");
-			
+
 			/*
 			 * Car
 			 */
@@ -62,46 +62,46 @@ public class NewPlayer {
 			write(w, "tank_record=0");
 			write(w, "tank_parties=0");
 			write(w, "tank_temps=0");
-			
+
 			write(w, "plainv_pts=0");
 			write(w, "plainv_parties=0");
 			write(w, "plainv_temps=0");
-			
+
 			write(w, "speedrun_record=0");
 			write(w, "speedrun_parties=0");
 			write(w, "speedrun_temps=0");
-			
+
 			write(w, "ping_control_bas=" + String.valueOf(Keyboard.KEY_DOWN));
 			write(w, "ping_control_haut=" + String.valueOf(Keyboard.KEY_UP));
 			write(w, "ping_control_mouse=2");
-			
+
 			write(w, "car_control_gauche=" + String.valueOf(Keyboard.KEY_LEFT));
-			write(w, "car_control_droite=" +String.valueOf(Keyboard.KEY_RIGHT));
-			
-			write(w, "snake_control_bas="+ String.valueOf(Keyboard.KEY_DOWN));
-			write(w, "snake_control_haut="+ String.valueOf(Keyboard.KEY_UP));
+			write(w, "car_control_droite=" + String.valueOf(Keyboard.KEY_RIGHT));
+
+			write(w, "snake_control_bas=" + String.valueOf(Keyboard.KEY_DOWN));
+			write(w, "snake_control_haut=" + String.valueOf(Keyboard.KEY_UP));
 			write(w, "snake_control_gauche=" + String.valueOf(Keyboard.KEY_LEFT));
 			write(w, "snake_control_droite=" + String.valueOf(Keyboard.KEY_RIGHT));
-			
+
 			write(w, "tank_control_bas=" + String.valueOf(Keyboard.KEY_DOWN));
-			write(w, "tank_control_haut=" +String.valueOf(Keyboard.KEY_UP));
-			
-			write(w, "speedrun_control_sneak=" +String.valueOf(Keyboard.KEY_DOWN));
+			write(w, "tank_control_haut=" + String.valueOf(Keyboard.KEY_UP));
+
+			write(w, "speedrun_control_sneak=" + String.valueOf(Keyboard.KEY_DOWN));
 			write(w, "speedrun_control_jump=" + String.valueOf(Keyboard.KEY_UP));
-			
-			write(w, "placeinvader_control_gauche="+ String.valueOf(Keyboard.KEY_LEFT));
+
+			write(w, "placeinvader_control_gauche=" + String.valueOf(Keyboard.KEY_LEFT));
 			write(w, "placeinvader_control_droite=" + String.valueOf(Keyboard.KEY_RIGHT));
-			
+
 			write(w, "sound_volume=5");
 			write(w, "sound=true");
-			
+
 			w.close();
-			
+
 		} catch (Exception e) {
-		
+
 		}
 	}
-
+	
 	private void write(final Writer w, final String s) {
 		try {
 			w.write(Main.getEncryptor().encrypt(s));
