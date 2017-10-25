419f74a7f601061fb0b9862176eaf69d77c53cb5
_Roro <roro51230@hotmail.fr>
2017-10-25 21:07:10 +0200

    Road to public

--- net/epopy/network/Test.java
+++ net/epopy/network/Test.java
@@ -3,8 +3,6 @@ package net.epopy.network;
 public class Test {
 
 	public static void main(String[] args) {
-		String myName = "$2y$10$kb2wu";
-		myName = myName.substring(0,2)+'a'+myName.substring(3);  
-		System.out.print(myName);
+	
 	}
 }
