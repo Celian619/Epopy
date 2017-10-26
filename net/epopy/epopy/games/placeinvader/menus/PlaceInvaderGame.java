b455b1aa20cc99fd157c7075668af682ecd877f1
Celi0n <celian.g@orange.fr>
2017-10-26 14:15:46 +0200

    collision + musique tetris

--- net/epopy/epopy/games/placeinvader/menus/PlaceInvaderGame.java
+++ net/epopy/epopy/games/placeinvader/menus/PlaceInvaderGame.java
@@ -24,46 +24,46 @@ import net.epopy.epopy.utils.Input;
 import net.epopy.epopy.utils.Location;
 
 public class PlaceInvaderGame extends AbstractGameMenu {
-
+	
 	private static final int robotSize = 80;
 	private static final int playerSize = 70;
 	private static final int yPlayer = defaultHeight - 100;
 	private double xPlayer;
-
+	
 	private boolean playerShot;
 	private int level;
 	private int scoreLevel;
 	private int score;
 	private int life;
-
+	
 	private int shooted;
-
+	
 	private List<robot> robots;
 	private List<Location> locBalleP;
 	private List<Location> locBalleR;
 	private boolean stats;
 	private static int timer;
 	private boolean pauseScreen;
-
+	
 	@Override
 	public void onEnable() {
 		if (Main.getPlayer().hasSound() && !Audios.PLACEINVADER.isRunning())
 			Audios.PLACEINVADER.start(true).setVolume(0.2f);
-
+			
 		playerShot = gameOver = stats = false;
 		shooted = scoreLevel = score = timer = 0;
 		level = 1;
 		life = 3;
 		robots = new ArrayList<robot>();
-
+		
 		locBalleP = new ArrayList<Location>();
 		locBalleR = new ArrayList<Location>();
-
+		
 		xPlayer = defaultWidth / 2;
 		pause.startPause(5);
 		Mouse.setGrabbed(true);
 	}
-
+	
 	@Override
 	public void update() {
 		if (!pauseScreen && pause.isFinish() && !gameOver) {
@@ -81,13 +81,13 @@ public class PlaceInvaderGame extends AbstractGameMenu {
 				}
 			}
 		}
-		
+
 		if (gameOver) {
 			if (rejouerButton.isClicked())
 				onEnable();
 			return;
 		}
-
+		
 		if (pauseScreen) {
 			if (reprendreButton.isClicked()) {
 				pause.startPause(3);
@@ -95,41 +95,41 @@ public class PlaceInvaderGame extends AbstractGameMenu {
 				Mouse.setGrabbed(true);
 			}
 		}
-
-		if (!pause.isFinish() || pauseScreen) return;
 		
+		if (!pause.isFinish() || pauseScreen) return;
+
 		if (life < 0) {
 			gameOver = true;
 			Mouse.setGrabbed(false);
 		}
-
+		
 		if (Input.isKeyDown(PlaceInvaderOptions.KEY_RIGHT)) {// aller a droite
-
+			
 			xPlayer += 10;
 			if (xPlayer + playerSize / 2 > defaultWidth) {
 				xPlayer = defaultWidth - playerSize / 2;
 			}
-
+			
 		}
 		if (Input.isKeyDown(PlaceInvaderOptions.KEY_LEFT)) {// aller a gauche
-
+			
 			xPlayer -= 10;
 			if (xPlayer < playerSize / 2) {
 				xPlayer = playerSize / 2;
 			}
-
+			
 		}
-
+		
 		if (Input.isButtonDown(0)) {// tirer
 			if (!playerShot) {
 				playerShot = true;
-
+				
 				locBalleP.add(new Location(xPlayer - 2, yPlayer));
 			}
 		} else if (playerShot) {
 			playerShot = false;
 		}
-
+		
 		for (int i = locBalleR.size() - 1; i >= 0; i--) {// si le joueur se fait shoot
 			Location locBR = locBalleR.get(i);
 			if (Math.abs(xPlayer - locBR.getX()) + Math.abs(yPlayer - locBR.getY()) <= playerSize / 2) {
@@ -141,7 +141,7 @@ public class PlaceInvaderGame extends AbstractGameMenu {
 				break;
 			}
 		}
-
+		
 		for (int i = locBalleP.size() - 1; i >= 0; i--) {
 			Location loc = locBalleP.get(i);
 			if (loc.getY() <= 1) {
@@ -151,9 +151,9 @@ public class PlaceInvaderGame extends AbstractGameMenu {
 				locBalleR.add(loc);// rebonds du joueur
 			} else
 				loc.remove(0, 10);
-
+				
 		}
-
+		
 		if (!locBalleR.isEmpty())
 			for (int i = locBalleR.size() - 1; i >= 0; i--) {
 			Location loc = locBalleR.get(i);
@@ -161,57 +161,57 @@ public class PlaceInvaderGame extends AbstractGameMenu {
 				locBalleR.remove(i);
 			} else
 				loc.add(0, 10);
-
+				
 		}
-
+		
 		if (robots.size() + scoreLevel < level && new Random().nextInt(30) == 1) new robot();
-
+		
 		for (int i = robots.size() - 1; i >= 0; i--) {
 			if (robots.isEmpty()) break;
 			robots.get(i).update();
 		}
-
+		
 		if (scoreLevel == level) {
 			level++;
 			scoreLevel = 0;
 		}
 		if (shooted > 0) shooted--;
 	}
-
+	
 	@Override
 	public void render() {
 		Textures.PLACEINVADER_LEVEL_BG.renderBackground();
-
+		
 		if (shooted > 0 && (shooted - 5) / 10 == shooted / 10) glColor4f(1, 0.2f, 0.2f, 1);
 		renderTexture(Textures.PLACEINVADER_ROCKET, xPlayer - playerSize / 2, yPlayer, playerSize, 100);
 		glColor4f(1, 1, 1, 1);
-
+		
 		if (pauseScreen) {
 			renderEchap(true);
 			return;
 		} else if (gameOver) {
 			renderTexture(Textures.PLACEINVADER_EXPLOSION, xPlayer - playerSize / 2 - 18, yPlayer, 100, 100);
-
+			
 			boolean record = false;
 			if (!stats) {
 				stats = true;
 				PlaceInvaderStats placeInvaderStats = Main.getPlayer().getPlaceInvaderStats();
 				placeInvaderStats.addPartie();
 				placeInvaderStats.addTemps(timer / 60);
-				
+
 				if (score >= placeInvaderStats.getRecord()) {
 					placeInvaderStats.setRecord(score);
 					record = true;
 				}
-
+				
 				if (score >= placeInvaderStats.getObjectif()) {
 					if (Main.getPlayer().getLevel() <= GameList.PLACEINVADER.getID())
 						Main.getPlayer().setLevel(GameList.PLACEINVADER.getID() + 1);
 				}
 			}
-
+			
 			renderEchap(false, score + "", record);
-
+			
 			if (rejouerButton.isClicked())
 				onEnable();
 			return;
@@ -221,33 +221,33 @@ public class PlaceInvaderGame extends AbstractGameMenu {
 				return;
 			}
 			if (pause.getTimePauseTotal() == 5) {
-				
-				Textures.GAME_STARTING_BG.renderBackground();
 
+				Textures.GAME_STARTING_BG.renderBackground();
+				
 				float[] orange = new float[] { 1, 0.5f, 0, 1 };
 				float[] white = new float[] { 1, 1, 1, 1 };
 				float[] grey = new float[] { 0.8f, 0.8f, 0.8f, 1 };
 				drawText("CONTROLES", 1093, 360, PosWidth.MILIEU, PosHeight.MILIEU, 30, orange);
 				drawText("Droite", 1013, 405, PosWidth.MILIEU, PosHeight.HAUT, 25, white);
 				drawText("Gauche", 1013, 550, PosWidth.MILIEU, PosHeight.HAUT, 25, white);
-
+				
 				drawText(Input.getKeyName(PlaceInvaderOptions.KEY_RIGHT), 985, 440, 50, white);
 				drawText(Input.getKeyName(PlaceInvaderOptions.KEY_LEFT), 985, 580, 50, white);
-
+				
 				drawText("Tir", 1093 + 80, 400 + 5, PosWidth.MILIEU, PosHeight.HAUT, 25, white);
-
+				
 				renderTexture(Textures.GAME_GAUCHE_SOURIS, 1123, 445, 100, 100);
-
+				
 				drawText("OBJECTIF", 660, 495, PosWidth.GAUCHE, PosHeight.HAUT, 30, orange);
 				drawText("Obtenir plus de", 710, 600, PosWidth.MILIEU, PosHeight.HAUT, 25, grey);
 				drawText("100 points.", 710, 630, PosWidth.MILIEU, PosHeight.HAUT, 25, grey);
-
+				
 				drawText(pause.getPauseString(), 660, 335, PosWidth.GAUCHE, PosHeight.HAUT, 100, white);
 				return;
 			} else
 				pause.showRestartChrono();
 		}
-
+		
 		for (int i = robots.size() - 1; i >= 0; i--) {
 			if (robots.isEmpty()) break;
 			robot rob = robots.get(i);
@@ -257,20 +257,20 @@ public class PlaceInvaderGame extends AbstractGameMenu {
 				renderTexture(Textures.PLACEINVADER_EXPLOSION, rob.loc.getX() - robotSize / 2, rob.loc.getY() - robotSize / 2, robotSize + 10, robotSize);
 			}
 		}
-
+		
 		for (Location loc : locBalleP)
 			drawLine(loc.getX(), loc.getY(), loc.getX(), loc.getY() - 30, 5, new float[] { 0, 1, 0, 1 });
-
+			
 		for (Location loc : locBalleR)
 			drawLine(loc.getX(), loc.getY(), loc.getX(), loc.getY() + 30, 5, new float[] { 1, 0, 0, 1 });
-
+			
 		drawText(score + " point" + (score < 2 ? "" : "s"), 30, 30, 30);
-
+		
 		for (int i = 0; i < life; i++)
 			renderTexture(Textures.PLACEINVADER_ROCKET, defaultWidth - (65 + i * 60), 40, 35, 50);
-
+			
 	}
-
+	
 	public class robot {
 		private static final double speedRobot = 0.8;
 		private final Random r = new Random();
@@ -278,7 +278,7 @@ public class PlaceInvaderGame extends AbstractGameMenu {
 		Boolean left;
 		int limitX;
 		boolean dead;
-
+		
 		public robot() {
 			left = r.nextBoolean();
 			loc = new Location(r.nextInt(defaultWidth - robotSize * 2) + robotSize, r.nextInt(defaultHeight / 2));
@@ -286,18 +286,18 @@ public class PlaceInvaderGame extends AbstractGameMenu {
 			dead = false;
 			robots.add(this);
 		}
-
+		
 		public void update() {
 			double xR = loc.getX();
 			double yR = loc.getY();
-
+			
 			if (left) {
 				xR -= speedRobot;
 				if (xR < limitX || xR < 0 + robotSize) {
 					left = false;
 					limitX = (int) (xR + r.nextInt(500));
 				}
-
+				
 			} else {
 				xR += speedRobot;
 				if (xR > limitX || xR + robotSize > defaultWidth) {
@@ -306,14 +306,14 @@ public class PlaceInvaderGame extends AbstractGameMenu {
 				}
 			}
 			yR += speedRobot;
-
+			
 			loc.setPos(xR, yR);
-
+			
 			if (yR > defaultHeight - playerSize) {
 				robots.clear();
 				life--;
 			}
-
+			
 			for (int i = locBalleP.size() - 1; i >= 0; i--) {
 				Location locBP = locBalleP.get(i);
 				if (loc.distance(locBP) <= robotSize / 2) {
@@ -323,14 +323,14 @@ public class PlaceInvaderGame extends AbstractGameMenu {
 					scoreLevel++;
 				}
 			}
-
+			
 			if (r.nextInt(200) == 1) {// tire !
 				locBalleR.add(new Location(loc.getX() - 1, loc.getY() + 10));
-
+				
 			}
-
+			
 		}
-
+		
 	}
-
+	
 }
